e12446b26febc84e943af4d9a9132e2da6787387
nguyen huy <quanghuy2371998@gmail.com>
2023-09-05 15:53:36 +0700

    NangCao

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import ListProduct from "./components/ListProduct";
+function App() {
+  return (
+    <>
+      <Home />
+      {/* <Cart /> */}
+      {/* <ListProduct /> */}
+    </>
+  );
+}
+
+export default App;
